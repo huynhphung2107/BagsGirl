a297ace1082be4d52f2b204e126ed4a1f3b63520
QuanNguyen210 <quanchua02102003@gmail.com>
2023-08-02 19:58:05 +0700

    Update rest API brand

--- src/main/java/fpoly/datn/ecommerce_website/repository/IBrandRepository.java
+++ src/main/java/fpoly/datn/ecommerce_website/repository/IBrandRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface IBrandRepository extends JpaRepository<Brand, UUID> {
+public interface IBrandRepository extends JpaRepository<Brand, String> {
 }
