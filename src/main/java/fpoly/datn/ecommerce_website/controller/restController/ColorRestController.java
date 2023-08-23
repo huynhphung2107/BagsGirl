544d2f3fe085d0b1ded6e84f026f0517c88d272f
oanhnk <118040842+oanhnk@users.noreply.github.com>
2023-08-23 21:31:47 +0700

    Complete

--- src/main/java/fpoly/datn/ecommerce_website/controller/restController/ColorRestController.java
+++ src/main/java/fpoly/datn/ecommerce_website/controller/restController/ColorRestController.java
@@ -1,61 +1,78 @@
 package fpoly.datn.ecommerce_website.controller.restController;
 
 
+import fpoly.datn.ecommerce_website.dto.ColorDTO;
+import fpoly.datn.ecommerce_website.dto.SizeDTO;
 import fpoly.datn.ecommerce_website.entity.Color;
-import fpoly.datn.ecommerce_website.repository.IColorReponsitory;
+
+import fpoly.datn.ecommerce_website.service.serviceImpl.ColorServiceImpl;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RequestMapping("/api/manage")
 @RestController
 public class ColorRestController {
 
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Autowired
-    private IColorReponsitory iColorReponsitory;
+    private ColorServiceImpl colorService;
 
-    @GetMapping(value = "/color")
-    public ResponseEntity<List<Color>> getAll() {
-        return new ResponseEntity<>(iColorReponsitory.findAll(), HttpStatus.OK);
+    @RequestMapping(value = "/color/", method = RequestMethod.GET)
+    public ResponseEntity<List<ColorDTO>> getAll() {
+        return new ResponseEntity<>(
+                this.colorService.findAll()
+                        .stream()
+                        .map(color -> modelMapper.map(color, ColorDTO.class))
+                        .collect(Collectors.toList())
+                , HttpStatus.OK
+        );
     }
 
-    @GetMapping(value = "/color/{id}")
-    public ResponseEntity<Color> getOne(@PathVariable("id") UUID id) {
-        Color color = iColorReponsitory.findById(id).orElse(null);
-        return new ResponseEntity<>(color, HttpStatus.OK);
+    @RequestMapping(value = "/color", method = RequestMethod.GET)
+    public ResponseEntity<ColorDTO> getOne(@RequestParam String id) {
+        return new ResponseEntity<>(
+                modelMapper.map(this.colorService.findById(id), ColorDTO.class)
+                , HttpStatus.OK);
     }
 
-    //update
-    @PutMapping(value = "/color")
-    public ResponseEntity<Color> update(@RequestBody Color color) {
-
-        return new ResponseEntity<>(iColorReponsitory.save(color), HttpStatus.OK);
+    @RequestMapping(value = "/color", method = RequestMethod.POST)
+    public ResponseEntity<Color> save(@RequestBody ColorDTO colorDTO) {
+        Color color = modelMapper.map(colorDTO, Color.class);
+        return new ResponseEntity<>(
+                this.colorService.save(color)
+                , HttpStatus.OK);
     }
 
-    //    addd
-    @PostMapping(value = "/color")
-    public ResponseEntity<Color> add(@RequestBody Color color) {
-        return new ResponseEntity<>(iColorReponsitory.save(color), HttpStatus.OK);
+    @RequestMapping(value = "/color", method = RequestMethod.PUT)
+    public ResponseEntity<Color> update(@RequestBody ColorDTO colorDTO) {
+        Color color = modelMapper.map(colorDTO, Color.class);
+        return new ResponseEntity<>(
+                this.colorService.save(color)
+                , HttpStatus.OK
+        );
     }
 
-    //delete
-    @DeleteMapping(value = "/color/{id}")
-    public ResponseEntity<?> remove(@PathVariable("id") UUID id) {
-        iColorReponsitory.deleteById(id);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @RequestMapping(value = "/color", method = RequestMethod.DELETE)
+    public ResponseEntity<?> delete(@RequestParam String id) {
+        this.colorService.delete(id);
+        return new ResponseEntity<>(
+                "Delete Successfully"
+                , HttpStatus.OK
+        );
     }
-
-
 }
+
+
