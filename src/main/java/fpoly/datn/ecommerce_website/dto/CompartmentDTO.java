d4b26183a08599a00916979130adcebe3dc33d2b
huynhphung2107 <huynhphung2107@gmail.com>
2023-08-24 19:37:10 +0700

    Update + Validate class Compartment và BuckleType

---
+++ src/main/java/fpoly/datn/ecommerce_website/dto/CompartmentDTO.java
@@ -0,0 +1,18 @@
+package fpoly.datn.ecommerce_website.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CompartmentDTO {
+
+    private String id;
+
+    @NotBlank(message = "Không để trống code")
+    private String code;
+
+    @NotBlank(message = "Không để trống name")
+    private String name;
+}
