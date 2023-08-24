759d2c78c1c7bae453007912031ea91a7a04620d
NguyenCongTuanAnh <tuanem051203@gmail.com>
2023-08-24 12:11:20 +0700

    Done Balo API

--- src/main/java/fpoly/datn/ecommerce_website/controller/restController/BaloRestController.java
+++ src/main/java/fpoly/datn/ecommerce_website/controller/restController/BaloRestController.java
@@ -1,7 +1,9 @@
 package fpoly.datn.ecommerce_website.controller.restController;
 
+import fpoly.datn.ecommerce_website.dto.BaloDTO;
 import fpoly.datn.ecommerce_website.entity.Balo;
 import fpoly.datn.ecommerce_website.repository.IBaloRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,35 +13,52 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.stream.Collectors;
+
 @RequestMapping("/api/manage")
 @RestController
 public class BaloRestController {
 
+    @Autowired
+    private ModelMapper modelMapper;
     @Autowired
     private IBaloRepository baloRepository;
 
     //hienthi
     @RequestMapping(value = "/balo/", method = RequestMethod.GET)
     public ResponseEntity<?> getAll() {
-        return new ResponseEntity<>(this.baloRepository.findAll(), HttpStatus.OK);
+        return new ResponseEntity<>(
+                this.baloRepository.findAll().stream().map(
+                        balo -> modelMapper.map(balo, Balo.class)
+                ).collect(Collectors.toList())
+                , HttpStatus.OK);
     }
 
     //hien thi get one
     @RequestMapping(value = "/balo", method = RequestMethod.GET)
     public ResponseEntity<?> getOne(@RequestParam("id") String id) {
-        return new ResponseEntity<>(this.baloRepository.findById(id).get(), HttpStatus.OK);
+        return new ResponseEntity<>(
+                modelMapper.map(this.baloRepository.findById(id).get(), BaloDTO.class)
+                , HttpStatus.OK);
     }
 
     //add
     @RequestMapping(value = "/balo", method = RequestMethod.POST)
-    public ResponseEntity<?> add(@RequestBody Balo balo) {
-        return new ResponseEntity<>(baloRepository.save(balo), HttpStatus.OK);
+    public ResponseEntity<?> add(@RequestBody BaloDTO baloDTO) {
+        return new ResponseEntity<>(baloRepository.save(
+                modelMapper.map(baloDTO, Balo.class)
+        )
+
+                , HttpStatus.OK);
     }
 
     //update
     @RequestMapping(value = "/balo", method = RequestMethod.PUT)
-    public ResponseEntity<?> update(@RequestBody Balo balo) {
-        return new ResponseEntity<>(baloRepository.save(balo), HttpStatus.OK);
+    public ResponseEntity<?> update(@RequestBody BaloDTO baloDTO) {
+        return new ResponseEntity<>(baloRepository.save(
+                modelMapper.map(baloDTO, Balo.class)
+
+        ), HttpStatus.OK);
     }
 
     //delete
---
+++ src/main/java/fpoly/datn/ecommerce_website/dto/BaloDTO.java
@@ -0,0 +1,22 @@
+package fpoly.datn.ecommerce_website.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class BaloDTO {
+
+    private String id;
+    private String code;
+    private String name;
+    private String status;
+}
