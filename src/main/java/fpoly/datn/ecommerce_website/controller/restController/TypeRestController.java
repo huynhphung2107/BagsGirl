3f9501d3924b59b324014a4d84862dd619d44c29
NguyenCongTuanAnh <tuanem051203@gmail.com>
2023-08-25 13:11:54 +0700

    Done Validate TypeRestController BY TA

--- src/main/java/fpoly/datn/ecommerce_website/controller/restController/TypeRestController.java
+++ src/main/java/fpoly/datn/ecommerce_website/controller/restController/TypeRestController.java
@@ -7,13 +7,19 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -71,4 +77,19 @@ public class TypeRestController {
                 "Delete Successfuly"
                 , HttpStatus.OK);
     }
+
+    //Validation Type Rest API
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMesssage = error.getDefaultMessage();
+            errors.put(fieldName, errorMesssage);
+        });
+
+        return errors;
+    }
+    //End
 }
