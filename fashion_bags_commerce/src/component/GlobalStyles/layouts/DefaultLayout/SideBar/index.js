76844a440c6b2fd55e14ebf3c63d455a3a2df3ea
NguyenCongTuanAnh <tuanem051203@gmail.com>
2023-09-13 18:59:32 +0700

    edit layout admin by ta

--- fashion_bags_commerce/src/component/GlobalStyles/layouts/DefaultLayout/SideBar/index.js
+++ fashion_bags_commerce/src/component/GlobalStyles/layouts/DefaultLayout/SideBar/index.js
@@ -1,19 +1,89 @@
-import { Fragment } from 'react';
-import items from '../SideBar/data.json';
-import SidebarItem from './SidebarItem/SidebarItem.js';
-import styles from './index.module.scss';
-import classNames from 'classnames/bind';
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Menu } from 'antd';
+import Avartar from '~/component/GlobalStyles/layouts/DefaultLayout/SideBar/Avartar';
+import { useState } from 'react';
 
-const cx = classNames.bind(styles);
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+const items = [
+  getItem('Navigation One', 'sub1', <MailOutlined />, [
+    getItem('Option 1', '1'),
+    getItem('Option 2', '2'),
+    getItem('Option 3', '3'),
+    getItem('Option 4', '4'),
+  ]),
+  getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
+    getItem('Option 5', '5'),
+    getItem('Option 6', '6'),
+    getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
+  ]),
+  getItem('Navigation Three', 'sub3', <SettingOutlined />, [
+    getItem('Option 9', '9'),
+    getItem('Option 10', '10'),
+    getItem('Option 11', '11'),
+    getItem('Option 12', '12'),
+  ]),
+  getItem('Navigation Three', 'sub4', <SettingOutlined />, [
+    getItem('Option 9', '9'),
+    getItem('Option 10', '10'),
+    getItem('Option 11', '11'),
+    getItem('Option 12', '12'),
+  ]),
+  getItem('Navigation Three', 'sub5', <SettingOutlined />, [
+    getItem('Option 9', '9'),
+    getItem('Option 10', '10'),
+    getItem('Option 11', '11'),
+    getItem('Option 12', '12'),
+  ]),
+  getItem('Navigation Three', 'sub6', <SettingOutlined />, [
+    getItem('Option 9', '9'),
+    getItem('Option 10', '10'),
+    getItem('Option 11', '11'),
+    getItem('Option 12', '12'),
+  ]),
+  getItem('Navigation Three', 'sub7', <SettingOutlined />, [
+    getItem('Option 9', '9'),
+    getItem('Option 10', '10'),
+    getItem('Option 11', '11'),
+    getItem('Option 12', '12'),
+  ]),
+];
+const rootSubmenuKeys = ['sub1', 'sub2', 'sub3', 'sub4', 'sub5', 'sub6', 'sub7'];
 function Sidebar() {
+  const [openKeys, setOpenKeys] = useState(['sub1']);
+  const onOpenChange = (keys) => {
+    const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
+    if (latestOpenKey && rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+      setOpenKeys(keys);
+    } else {
+      setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+    }
+  };
   return (
-    <Fragment>
-      <div className={cx('sidebar')}>
-        {items.map((item, index) => (
-          <SidebarItem key={index} item={item} />
-        ))}
+    <div style={{ height: '100vh', zIndex: '999' }}>
+      <div style={{ padding: ' 0px 30px' }}>
+        <Avartar />
       </div>
-    </Fragment>
+      <Menu
+        mode="inline"
+        openKeys={openKeys}
+        onOpenChange={onOpenChange}
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        style={{
+          width: '260px',
+          height: 'auto',
+        }}
+        items={items}
+      />
+    </div>
   );
 }
 
--- fashion_bags_commerce/src/pages/Admin/index.js
+++ fashion_bags_commerce/src/pages/Admin/index.js
@@ -1,16 +1,60 @@
-import styles from '~/pages/Admin/index.module.scss';
-import classNames from 'classnames/bind';
 import Sidebar from '~/component/GlobalStyles/layouts/DefaultLayout/SideBar';
+import { Layout, Space, Menu } from 'antd';
+import Avartar from '~/component/GlobalStyles/layouts/DefaultLayout/SideBar/Avartar';
 
-const cx = classNames.bind(styles);
+const { Header, Footer, Sider, Content } = Layout;
+const layout = {
+  paddingLeft: '270px',
+};
+const headerStyle = {
+  borderLeft: '270px',
+  color: '#fff',
+  height: 'auto',
+  paddingInline: 0,
+  lineHeight: '64px',
+  backgroundColor: '#7dbcea',
+};
+const contentStyle = {
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#108ee9',
+};
+const footerStyle = {
+  borderLeft: '270px',
+  color: '#fff',
+  backgroundColor: '#7dbcea',
+};
+const layoutContent = {
+  marginLeft: '260px',
+  flexGrow: '1',
+};
+
 function Admin() {
   return (
-    <div className={cx('wrapper')}>
-      <Sidebar />
-      <Sidebar />
-
-      <div className={cx('content')}>content</div>
-    </div>
+    <Layout style={{ height: '100vh', background: '#f4f3f4' }}>
+      <Sider width={260} style={{ background: '#fff', zIndex: '999', position: 'fixed' }}>
+        <Sidebar />
+      </Sider>
+      <Layout style={layoutContent}>
+        <Header style={headerStyle}>
+          Đây là phần component của HeaderĐây là phần component của HeaderĐây là phần component của HeaderĐây là phần
+          component của HeaderĐây là phần component của HeaderĐây là phần component của HeaderĐây là phần component của
+          Header
+        </Header>
+        <Content style={contentStyle}>Đây là component của Content</Content>
+        <Content style={contentStyle}>Đây là component của Content</Content>
+        <Content style={contentStyle}>Đây là component của Content</Content>
+        <Content style={contentStyle}>Đây là component của Content</Content>
+        <Content style={contentStyle}>Đây là component của Content</Content>
+        <Content style={contentStyle}>Đây là component của Content</Content>
+        <Content style={contentStyle}>Đây là component của Content</Content>
+        <Content style={contentStyle}>Đây là component của Content</Content>
+        <Content style={contentStyle}>Đây là component của Content</Content>
+        <Content style={contentStyle}>Đây là component của Content</Content>
+        <Footer style={footerStyle}>Đây là component của Footer</Footer>
+      </Layout>
+    </Layout>
   );
 }
 
