abea02325de69580488c22b5c4ce9404e0e09b3e
HUYNHPHUNG\Huynh <huynhphung2107@gmail.com>
2023-09-25 22:16:17 +0700

    fix 1 xius

--- fashion_bags_commerce/src/App.js
+++ fashion_bags_commerce/src/App.js
@@ -5,12 +5,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Admin from './pages/Admin';
 import ProductViewer from './pages/Admin/ProductManager/ProductViewer';
-<<<<<<< HEAD
+
 import ShiftViewer from './pages/Admin/Shift/ShiftViewer';
 
-=======
+
 import Login from './pages/Login';
->>>>>>> main
+
 
 function App() {
   return (
@@ -21,11 +21,7 @@ function App() {
           <Route path="/login" element={<Login />}></Route>
           <Route path="/admin" element={<Admin />}></Route>
           <Route path="/product-viewer" element={<ProductViewer />}></Route>
-<<<<<<< HEAD
-          <Route path="/profile" element={<Profile />}></Route>
           <Route path="/shift" element={<ShiftViewer />}></Route>
-=======
->>>>>>> main
         </Routes>
       </div>
     </Router>
