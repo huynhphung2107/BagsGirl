cb9d3ff2f3b235bbf7c0144010384a8fce2f86b6
HUYNHPHUNG\Huynh <huynhphung2107@gmail.com>
2023-10-27 10:31:41 +0700

    no message

--- fashion_bags_commerce/src/pages/Admin/ThuocTinhSanPham/Brand/BrandEdit/FormEdit/FormBrandEdit.js
+++ fashion_bags_commerce/src/pages/Admin/ThuocTinhSanPham/Brand/BrandEdit/FormEdit/FormBrandEdit.js
@@ -1,122 +1,139 @@
-import React, { Fragment, useState, initialValue, useEffect } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { EditOutlined } from '@ant-design/icons';
-
-import { Button, Col, Modal, Form, Input, Row, Select, Space, notification } from 'antd';
+import { Button, Col, Drawer, Form, Input, Row, Select, Space, notification } from 'antd';
 import brandAPI from '~/api/propertitesBalo/brandAPI';
-// import styles from './FormBrandEdit.module.scss';
 
 function FormBrandEdit(props) {
-  const [modalOpen, setIsModalOpen] = useState(false);
-  const [error, setErrorText] = useState(true);
-  const [form] = Form.useForm();
+  const [open, setOpen] = useState(false);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState(props.brand);
+  const [stringStatus, setStringStatus] = useState('');
+  const [reloadTable, setReloadTable] = useState(false);
 
-  const showModal = () => {
-    setIsModalOpen(true);
+  const showComponent = () => {
+    setOpen(true);
+    if (data.brandStatus === 1) {
+      setStringStatus('Hoạt động');
+    } else if (data.brandStatus === -1) {
+      setStringStatus('Ngừng hoạt động');
+    } else {
+      setStringStatus('Không hoạt động');
+    }
   };
 
-  const handleOk = () => {
-    setIsModalOpen(false);
+  const closeComponent = () => {
+    setOpen(false);
   };
 
-  const handleCancel = () => {
-    setIsModalOpen(false);
+  const updateData = (event) => {
+    const { name, value } = event.target;
+    setData({ ...data, [name]: value });
+  };
+  const updateStatus = (value) => {
+    setData({ ...data, brandStatus: value });
   };
 
-  const updateFunc = async (props) => {
-    console.log(props);
-    setErrorText('');
-    try {
-      const response = await brandAPI.update(props);
-
-      notification.success({
-        message: 'Update thành công',
-        description: 'Dữ liệu đã được cập nhật thành công',
-        duration: 1,
-      });
-
-      return response.data;
-    } catch (error) {
-      const errorResponse = error.response;
-      if (errorResponse) {
-        setErrorText(errorResponse.data.message);
-      } else {
-        setErrorText('Có lỗi xảy ra khi gửi yêu cầu cập nhật.');
+  const updateFunction = async (brandId, values) => {
+    setError(false);
+    let update = { ...values };
+    console.log(brandId);
+    console.log(update);
+    if (!error) {
+      try {
+        await brandAPI.update(brandId, update);
+        notification.success({
+          message: 'Cập nhật thành công',
+          description: 'Dữ liệu đã được cập nhật thành công',
+          duration: 2,
+        });
+        closeComponent();
+      } catch (error) {
+        console.log(error);
+        setError(true);
+        notification.error({
+          message: 'Cập nhật thất bại',
+          description: 'Dữ liệu không được cập nhật',
+          duration: 2,
+        });
       }
-      notification.error({
-        message: 'Lỗi',
-        description: error.toString(),
-        duration: 2,
-      });
-      // console.log(error);
-
-      console.log(error);
     }
   };
 
   return (
     <Fragment>
-      <Button type="primary" className="btn btn-success" onClick={showModal} icon={<EditOutlined />}>
-        Edit
-      </Button>
-      <br></br>
-      <Modal title={'Edit - ' + props.brand.brandName} open={modalOpen} onCancel={handleCancel} footer={null}>
-        <div>
-          <Form
-            form={form}
-            labelCol={{
-              span: 8,
-            }}
-            style={{
-              maxWidth: 600,
-            }}
-            wrapperCol={{
-              span: 16,
-            }}
-            onFinish={updateFunc} // Xử lý khi submit form
-            initialValues={props.brand}
-          >
-            <Form.Item
-              label="Brand Name"
-              name="brandName"
-              rules={[
-                {
-                  required: true,
-                  message: 'Vui lòng điền Tên thương hiệu!',
-                },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-
-            <Form.Item label="Status" name="brandStatus">
-              <Select
-                style={{ width: 300 }}
-                placeholder="Vui lòng chọn trạng thái"
-                options={[
-                  {
-                    value: '1',
-                    label: 'Hoạt động',
-                  },
-                  {
-                    value: '0',
-                    label: 'Không Hoạt động',
-                  },
-                  {
-                    value: '-1',
-                    label: 'Ngừng Hoạt động',
-                  },
-                ]}
-              />
-            </Form.Item>
-
-            <div style={{ textAlign: 'center' }}>
-              <Button type="primary" onClick={updateFunc}>
-                Submit
+      <div style={{ color: 'red' }}>
+        <Button type="primary" className="btn btn-warning" onClick={showComponent} icon={<EditOutlined />}>
+          Edit
+        </Button>
+        <Drawer
+          title={'Edit - ' + data.brandId}
+          width={400}
+          onClose={closeComponent}
+          open={open}
+          style={{
+            paddingBottom: 80,
+          }}
+          extra={
+            <Space>
+              <Button onClick={closeComponent}>Thoát</Button>
+              <Button onClick={() => updateFunction(data.brandId, data)} type="primary" className="btn btn-warning">
+                Lưu
               </Button>
-            </div>
+            </Space>
+          }
+        >
+          <Form layout="vertical" hbrandIdeRequiredMark initialValues={data}>
+            <Row gutter={16}>
+              <Col span={24}>
+                <Form.Item
+                  name="brandCode"
+                  label="Mã"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Vui lòng điền Mã Kiểu',
+                    },
+                  ]}
+                >
+                  <Input placeholder="Vui lòng điền Mã Kiểu" disabled />
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col span={24}>
+                <Form.Item
+                  name="brandName"
+                  label="Tên"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Vui lòng điền tên Kiểu',
+                    },
+                  ]}
+                >
+                  <Input
+                    name="brandName"
+                    value={data.brandName}
+                    onChange={updateData}
+                    placeholder="Vui lòng điền tên kiểu"
+                  />
+                </Form.Item>
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col span={24}>
+                <Form.Item label="Trạng Thái">
+                  <Select onChange={updateStatus} defaultValue={stringStatus} placeholder="Vui lòng chọn Trạng Thái">
+                    <Select.Option value="1">Hoạt động</Select.Option>
+                    <Select.Option value="0">Không hoạt động</Select.Option>
+                    <Select.Option value="-1">Ngừng hoạt động</Select.Option>
+                  </Select>
+                </Form.Item>
+              </Col>
+            </Row>
           </Form>
-        </div>
-      </Modal>
+        </Drawer>
+      </div>
     </Fragment>
   );
 }
