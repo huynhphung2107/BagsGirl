f15fef72093b8de97ab531c92e56f2ed87035eea
HUYNHPHUNG\Huynh <huynhphung2107@gmail.com>
2023-11-03 09:12:54 +0700

    update giao diện

--- fashion_bags_commerce/src/pages/Home/Content/LayoutCarousel/index.js
+++ fashion_bags_commerce/src/pages/Home/Content/LayoutCarousel/index.js
@@ -29,29 +29,13 @@ function LayoutCarousel() {
         <div className={styles.container}>
           <div className="row">
             <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12 item-banner">
-              <a
-                href="https://www.vascara.com/tui-deo-cheo/tui-deo-cheo-phong-cach-denim-khoa-xoay-sho-0228-mau-nau"
-                aria-label="Bộ sưu tập vascara"
-              >
-                <img
-                  className={styles.imgContent}
-                  src="https://www.vascara.com/uploads/banner/2023/September/18/17221695020923.jpg"
-                  data-src=""
-                  alt="Bộ sưu tập vascara"
-                />
+              <a href="#" aria-label="Bộ sưu tập vascara">
+                <img className={styles.imgContent} src="https://www.vascara.com/uploads/cms_productmedia/2023/August/31/giay-slingback-nhan-quai-ankle-strap---bmn-0600---mau-do__71387__1693424341-medium.jpg" data-src="" alt="Bộ sưu tập vascara" />
               </a>
             </div>
             <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12 item-banner">
-              <a
-                href="https://www.vascara.com/giay-bit/giay-slingback-quai-khoet-cach-dieu-bmn-0597-mau-be"
-                aria-label="Bộ sưu tập vascara"
-              >
-                <img
-                  className={styles.imgContent}
-                  src="https://www.vascara.com/uploads/banner/2023/September/18/17241695021083.jpg"
-                  data-src=""
-                  alt="Bộ sưu tập vascara"
-                />
+              <a href="#" aria-label="Bộ sưu tập vascara">
+                <img className={styles.imgContent} src="https://www.vascara.com/uploads/cms_productmedia/2023/August/31/giay-slingback-nhan-quai-ankle-strap---bmn-0600---mau-do__71387__1693424341-medium.jpg" data-src="" alt="Bộ sưu tập vascara" />
               </a>
             </div>
           </div>
